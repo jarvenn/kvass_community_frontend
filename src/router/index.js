058c126cae0a30e9b21c3d80538265270e89a663
Hujiawen <545701101@qq.com>
2021-04-04 17:19:22 +0800

    侧边链接，用户注册，用户登录（vuex,js-cookie）

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,29 @@ const routes = [
         path: '/',
         name: 'Home',
         component: () => import('@/views/Home')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/auth/Register'),
+        meta: {title: '注册'}
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: () => import('@/views/auth/Login'),
+        meta: {title: '登录'}
+    },
+    {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error/404'),
+        meta: {title: '404-NotFound'}
+    },
+    {
+        path: '*',
+        redirect: '/404',
+        hidden: true
     }
 ]
 
